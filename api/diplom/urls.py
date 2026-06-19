432953d4fb4860ed2951077ddefb2e8b22c6e43f
Александр Витальевич <tip200017@gmail.com>
2026-06-19 17:44:19 +1000

    добавил policy.html, изменение в urls.py

--- api/diplom/urls.py
+++ api/diplom/urls.py
@@ -3,6 +3,7 @@ from . import views
 from .views import register_view
 from django.contrib import admin
 from django.contrib.auth.views import LogoutView
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,5 +15,6 @@ urlpatterns = [
     path('add-event/', views.add_event, name='add_event'),
     path('notes/', include('notes.urls')),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
-    path('tasks/delete/<int:task_id>/', views.delete_task, name='delete_task')
+    path('tasks/delete/<int:task_id>/', views.delete_task, name='delete_task'),
+    path('policy/', TemplateView.as_view(template_name='policy.html'), name='policy'),
 ]
