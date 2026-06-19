c1f6eb76e0b1c837c4031eab9757490b97192681
Александр Витальевич <tip200017@gmail.com>
2026-06-19 15:41:03 +1000

    еще одно изменение в settings.py

--- api/diplom/settings.py
+++ api/diplom/settings.py
@@ -152,5 +152,5 @@ USE_TZ = True
 STATIC_URL = 'static/'
 LOGIN_URL = '/login/'
 
-STATIC_ROOT = '/api/staticfiles_build/static'
+STATIC_ROOT = '/app/staticfiles_build/static'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
